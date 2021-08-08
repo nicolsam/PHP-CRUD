b2a1f5b22d4bcc255978eb7eb0acc1aed742e461
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-07 22:41:10 -0300

    Adicionado arquivos header e footer

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>qTrabalhos</title>
+  </head>
+  <body>
+    <div class="p-2">
+      <header class="bg-dark">
+        <h1>qTrabalhos</h1>
+        <p>Sua vaga te espera!</p>
+      </header>
+    </div>
+    
