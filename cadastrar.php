33103faa04c3da9066bcf154467e0d8d4a70004f
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-13 00:06:36 -0300

    Sistema temporário de rotas melhorado

--- cadastrar.php
+++ cadastrar.php
@@ -3,5 +3,5 @@
 require __DIR__ . '/vendor/autoload.php';
 
 include __DIR__ . '/includes/header.php';
-include __DIR__ . '/includes/listagem.php';
+include __DIR__ . '/includes/formulario.php';
 include __DIR__ . '/includes/footer.php';
