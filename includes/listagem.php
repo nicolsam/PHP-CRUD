422dfcf5ad60fbd27e422e432f529b12ead37552
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-15 19:17:10 -0300

    Adicionada lista de vagas

--- includes/listagem.php
+++ includes/listagem.php
@@ -4,4 +4,23 @@
             <button class="btn btn-success">Nova vaga</button>
         </a>
     </section>
+
+    <section>
+
+        <table class="table bg-dark text-light mt-3 ">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>TITULO</th>
+                    <th>DESCRIÇÃO</th>
+                    <th>STATUS</th>
+                    <th>DATA</th>
+                    <th>AÇÕES</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+
+    </section>
 </main>
