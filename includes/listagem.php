9b7c0fddb378463b9fb335adec3cd31c00bdaf38
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-15 22:49:09 -0300

    Atualização no visual da página

--- includes/listagem.php
+++ includes/listagem.php
@@ -3,14 +3,14 @@
     $resultados = '';
     foreach($vagas as $vaga) {
         $resultados .= "<tr>
-                            <td>".$vaga->id."</td>
-                            <td>".$vaga->titulo."</td>
-                            <td>".$vaga->descricao."</td>
-                            <td>".($vaga->status == 'sim' ? 'Ativo' : 'Inativo')."</td>
-                            <td>".date('d/m/Y à\s H:i:s', strtotime($vaga->data))."</td>
-                            <td>
+                            <td class='text-center'>".$vaga->id."</td>
+                            <td class='text-center'>".$vaga->titulo."</td>
+                            <td class='text-center'>".$vaga->descricao."</td>
+                            <td class='text-center'>".($vaga->status == 'sim' ? 'Ativo' : 'Inativo')."</td>
+                            <td class='text-center'>".date('d/m/Y à\s H:i:s', strtotime($vaga->data))."</td>
+                            <td class='text-center'>
                                 <a href='editar.php?id=".$vaga->id."'><button type='button' class='btn btn-warning'>Editar</button></a>
-                                <button type='button' class='btn btn-danger' data-bs-toggle='modal' data-bs-target='#excluir-vaga-".$vaga->id."'>Excluir ".$vaga->id."</button>
+                                <button type='button' class='btn btn-danger' data-bs-toggle='modal' data-bs-target='#excluir-vaga-".$vaga->id."'>Excluir</button>
                                 <div class='modal fade text-dark' id='excluir-vaga-".$vaga->id."' tabindex='-1' aria-labelledby='excluirModal' aria-hidden='true'>
                                     <div class='modal-dialog'>
                                         <div class='modal-content'>
@@ -45,12 +45,12 @@
         <table class="table bg-dark text-light mt-3 ">
             <thead>
                 <tr>
-                    <th>ID</th>
-                    <th>TITULO</th>
-                    <th>DESCRIÇÃO</th>
-                    <th>STATUS</th>
-                    <th>DATA</th>
-                    <th>AÇÕES</th>
+                    <th class="text-center">ID</th>
+                    <th class="text-center">TITULO</th>
+                    <th class="text-center">DESCRIÇÃO</th>
+                    <th class="text-center">STATUS</th>
+                    <th class="text-center">DATA</th>
+                    <th class="text-center">AÇÕES</th>
                 </tr>
             </thead>
             <tbody>
