5e18b6aa79a2be8a0e8725aca24328b4f9c764ed
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-15 21:24:17 -0300

    Index agora está carregando em uma variável todas as vagas existentes

--- index.php
+++ index.php
@@ -11,10 +11,6 @@ use \App\Entity\Vaga;
 
 $vagas = Vaga::getVagas();
 
-echo '<pre>';
-print_r($vagas);
-echo '</pre>'; exit;
-
 include __DIR__ . '/includes/header.php';
 include __DIR__ . '/includes/listagem.php';
 include __DIR__ . '/includes/footer.php';
