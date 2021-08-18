4b894a2d2504fcdb126ed6dcc26e47ce8dbe2312
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-17 23:11:28 -0300

    Adicionada estilização do elemento de pesquisa

--- includes/header.php
+++ includes/header.php
@@ -7,11 +7,15 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
+    <!-- Fontawesome --->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="includes/styles/borderless.css">
     <title>qTrabalhos</title>
   </head>
   <body>
     <div class="p-2">
-      <header class="bg-dark text-light p-2">
+      <header class="bg-dark text-light p-4 rounded">
         <h1 class="fw-bold">qTrabalhos</h1>
         <p>Sua vaga te espera!</p>
       </header>
